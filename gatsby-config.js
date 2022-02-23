050334c3cd97b34fb317b5a3d13ebd06b94624b7
Albert Álvarez-Carulla <hello@thealbert.dev>
2022-02-23 17:02:05 +0100

    Añade el plugin de ChakraUI para Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
     siteUrl: `https://masb.thealbert.dev`,
   },
   plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
