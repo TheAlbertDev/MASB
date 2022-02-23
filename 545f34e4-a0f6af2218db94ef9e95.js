7ed902633c5014772baacdf057d2de556745c847
TheAlbertDev <TheAlbertDev@users.noreply.github.com>
2022-02-23 15:13:36 +0000

    deploy: 9c91ca74317d505ae62cb2d9d16122900cc25865

---
+++ 545f34e4-a0f6af2218db94ef9e95.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[980],{3256:function(t,a,e){e.d(a,{vHB:function(){return u}});var r=e(5566);function u(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"}}]})(t)}}}]);
+//# sourceMappingURL=545f34e4-a0f6af2218db94ef9e95.js.map
